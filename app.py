14cbb74e3ea8cd1bbee5772181d64738cb64884a
“swapnilwadhankar9@gmail.com” <swapnilwadhankar9@gmail.com>
2023-01-23 20:07:12 +0530

    The project has been added

---
+++ app.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return "Welcome!"
+
+@app.route('/how are you')
+def hello():
+    return 'I am good, how about you?'
+
+@app.route('/tell me something')
+def something():
+    return 'You are the best! Keep going'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
